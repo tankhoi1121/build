e15c6702ce4f06de546ef0bd5f2e1c63735d8c8b
ASUS <ASUS@DESKTOP-12KOKFE>
2020-03-19 21:21:25 +0700

    My first commit

---
+++ precache-manifest.1e0a2f7ab9de32f85c6b3759ebfad672.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3965436bc0ee66c94def891084a5026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199219db65b32687a931",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "48f534ebf2405612b6a4",
+    "url": "/static/css/main.1c2cb1cd.chunk.css"
+  },
+  {
+    "revision": "199219db65b32687a931",
+    "url": "/static/js/2.e8fa24c0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e8fa24c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f534ebf2405612b6a4",
+    "url": "/static/js/main.8e97689f.chunk.js"
+  },
+  {
+    "revision": "17cfae02d49a60ffd23e",
+    "url": "/static/js/runtime-main.536becfb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
